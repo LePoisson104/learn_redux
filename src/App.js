923ac37c5999d78c2da3d33e26a56846e017ded7
Viet Pham <vietpham2017@gmail.com>
2024-08-21 03:25:42 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import NavBar from "./components/NavBar";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((store) => store.cart);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main className="App">
       <NavBar />
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -15,8 +15,35 @@ const cartSlice = createSlice({
     clearCart: (state) => {
       state.cartItems = [];
     },
+    removeItem: (state, action) => {
+      console.log(action);
+      const itemId = action.payload;
+      // filter to make a new list that only contains the items that does not have the same id as the removed item
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increment: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount += 1;
+    },
+    decrement: (state, { payload }) => {
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      cartItem.amount -= 1;
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      // loop through the cartItems to get the amount and the price to add up the total
+      state.cartItems.forEach((item) => {
+        state.amount += item.amount;
+        state.total += item.amount * item.price;
+      });
+
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItem, increment, decrement, calculateTotals } =
+  cartSlice.actions;
 export default cartSlice.reducer;
